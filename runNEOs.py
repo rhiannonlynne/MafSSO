9cd3e9ba40ecdf659f785abe5c57827c21095410
Lynne Jones <ljones.uw@gmail.com>
2015-08-06 17:57:38 -1000

    updated Neo script

--- runNEOs.py
+++ runNEOs.py
@@ -1,4 +1,4 @@
-import os
+import os, argparse
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -41,55 +41,72 @@ def calcCompleteness(orbitfile, obsfile, outDir, runName, metadata=None):
 
     # Calculate completeness. First we must calculate "DiscoveryChances".
     # Set up an example metric bundle.
-    metric = moMetrics.DiscoveryChancesMetric()
-    slicer = mos
-    pandasConstraint = None
-    discovery = mmb.MoMetricBundle(metric, slicer, pandasConstraint,
-                                    runName=runName, metadata=metadata, plotDict=plotDict)
+    discovery = {}
+    nObsList = [2, 3, 4]
+    for nObs in nObsList:
+        md = metadata + ' %d visits/night' %(nObs)
+        metric = moMetrics.DiscoveryChancesMetric(nObsPerNight=nObs, Hindex=0.3)
+        slicer = mos
+        pandasConstraint = None
+        discovery[nObs] = mmb.MoMetricBundle(metric, slicer, pandasConstraint,
+                                        runName=runName, metadata=md, plotDict=plotDict)
 
+    bdict = {'discovery_%s' %(nObs):discovery[nObs] for nObs in nObsList }
+    bdict['nobs'] = nobs
 
-    bdict = {'nobs':nobs, 'discovery':discovery}
     bg = mmb.MoMetricBundleGroup(bdict, outDir=outDir)
     bg.runAll()
     bg.plotAll()
 
     ph = plots.PlotHandler(outDir=outDir)
     # Then calculate 'completeness' as function of H, as a secondary metric.
-    completeness = discovery.reduceMetric(discovery.metric.reduceFuncs['Completeness'])
-
-    # And we can make an 'integrated over H distribution' version.
-    completenessInt = completeness.reduceMetric(completeness.metric.reduceFuncs['CumulativeH'])
+    completeness = {}
+    completenessInt = {}
+    for nObs in nObsList:
+        completeness[nObs] = discovery[nObs].reduceMetric(discovery[nObs].metric.reduceFuncs['Completeness'])
+        # And we can make an 'integrated over H distribution' version.
+        completenessInt[nObs] = completeness[nObs].reduceMetric(completeness[nObs].metric.reduceFuncs['CumulativeH'])
 
     Hmark = 22.0
-    for c in [completeness, completenessInt]:
-        summaryMetric = ValueAtHMetric(Hmark=Hmark)
-        c.setSummaryMetrics(summaryMetric)
-        c.computeSummaryStats()
-        label = "Completeness at H=%.1f: %.2f" %(Hmark, c.summaryValues['Value At H=%.1f' %Hmark])
-        c.setPlotDict({'label':label})
-        c.plot(plotFunc = moPlots.MetricVsH())
-        plt.axvline(Hmark, color='r', linestyle=':')
-        plt.axhline(c.summaryValues['Value At H=%.1f' %(Hmark)], color='r', linestyle='-')
-        plt.legend(loc=(0.1, 0.2))
-        plt.savefig(os.path.join(outDir, c.fileRoot + '_vsH' + '.png'), format='png')
+    for nObs in nObsList:
+        for c in [completeness[nObs], completenessInt[nObs]]:
+            summaryMetric = ValueAtHMetric(Hmark=Hmark)
+            c.setSummaryMetrics(summaryMetric)
+            c.computeSummaryStats()
+            label = "Completeness at H=%.1f: %.2f" %(Hmark, c.summaryValues['Value At H=%.1f' %Hmark])
+            c.setPlotDict({'label':label})
+            c.plot(plotFunc = moPlots.MetricVsH())
+            plt.axvline(Hmark, color='r', linestyle=':')
+            plt.axhline(c.summaryValues['Value At H=%.1f' %(Hmark)], color='r', linestyle='-')
+            plt.legend(loc=(0.1, 0.2))
+            plt.savefig(os.path.join(outDir, c.fileRoot + '_vsH' + '.png'), format='png')
 
 
 if __name__ == '__main__':
 
-    outDir = 'pha_enigma_1189'
+    parser = argparse.ArgumentParser(description='Python script to run MAF NEO completeness analysis')
+    parser.add_argument('dbFile', type=str, default=None,help="full file path to the opsim sqlite file")
+    parser.add_argument('--orbitfile', type=str, default='pha20141031.des', help="full file path to the orbit file")
+    parser.add_argument("--outDir",type=str, default='./Out', help='Output directory for MAF outputs. Default "Out"')
+    parser.add_argument('--plotOnly', dest='plotOnly', action='store_true',
+                        default=False, help="Reload the metric values from disk and re-plot them.")
+    parser.set_defaults()
+    args, extras = parser.parse_known_args()
+
+    outDir = args.outDir
     if not os.path.isdir(outDir):
         os.makedirs(outDir)
-    orbitfile = 'pha20141031.des'
-    obsfile = os.path.join(outDir, 'pha_obs.txt')
-    opsimdb = 'enigma_1189_sqlite.db'
-    runName = 'enigma_1189'
-    metadata = None
+    orbitfile = args.orbitfile
+    orbitroot = orbitfile.replace('.txt', '').replace('.des', '').replace('.dat', '')
+    obsfile = os.path.join(outDir, orbitroot + '_allObs.txt')
+    opsimdb = args.dbFile
+    runName = os.path.split(opsimdb)[-1].replace('_sqlite.db', '')
+    metadata = '%s' %(orbitroot)
     rFov = np.radians(1.75)
     useCamera = True
 
-
-    makeObs = False
-    if makeObs:
+    if not args.plotOnly:
         makeObs(orbitfile, obsfile, opsimdb, rFov, useCamera)
+        print 'Made observations'
     calcCompleteness(orbitfile, obsfile, outDir, runName, metadata)
     print "All Done!"
