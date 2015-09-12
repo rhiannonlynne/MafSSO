68b24b1a168623d2b4195212bda9e2a184c99668
Lynne Jones <lynnej@lucy.local>
2015-09-12 09:06:18 -0700

    Updated the runNEOs script to match new completeness

--- runNEOs.py
+++ runNEOs.py
@@ -22,6 +22,7 @@ def makeObs(orbitfile, obsfile, opsimdb, rFov, useCamera):
     # useCamera = True/False (use camera footprint)
     moObs.runMoObs(orbitfile, obsfile, opsimdb, rFov=rFov, useCamera=useCamera, tstep=2./24.)
 
+
 def calcCompleteness(orbitfile, obsfile, outDir, runName, metadata=None):
     """
     Calculate a basic set of metrics on the NEOs, including completeness.
@@ -45,55 +46,63 @@ def calcCompleteness(orbitfile, obsfile, outDir, runName, metadata=None):
     nObsList = [2, 3, 4]
     for nObs in nObsList:
         md = metadata + ' %d visits/night' %(nObs)
-        metric = moMetrics.DiscoveryChancesMetric(nObsPerNight=nObs, Hindex=0.3)
+        metric = moMetrics.DiscoveryMetric(nObsPerNight=nObs)
         slicer = mos
         pandasConstraint = None
         discovery[nObs] = mmb.MoMetricBundle(metric, slicer, pandasConstraint,
-                                        runName=runName, metadata=md, plotDict=plotDict)
+                                             runName=runName, metadata=md, plotDict=plotDict)
+
+    discovery['4nights'] = mmb.MoMetricBundle(metric=moMetrics.DiscoveryMetric(nObsPerNight=2, nNightsPerWindow=4),
+                                              slicer=mos, pandasConstraint=None, runName=runName, 
+                                              metadata = metadata+'4 nights/track', plotDict=plotDict)
 
     bdict = {'discovery_%s' %(nObs):discovery[nObs] for nObs in nObsList }
+    bdict['4nights'] = discovery['4nights']
     bdict['nobs'] = nobs
 
     bg = mmb.MoMetricBundleGroup(bdict, outDir=outDir)
     bg.runAll()
     bg.plotAll()
 
-    ph = plots.PlotHandler(outDir=outDir)
-    # Then calculate 'completeness' as function of H, as a secondary metric.
     completeness = {}
     completenessInt = {}
+    hVals = {}
+    plt.figure()
     for nObs in nObsList:
-        completeness[nObs] = discovery[nObs].reduceMetric(discovery[nObs].metric.reduceFuncs['Completeness'])
-        # And we can make an 'integrated over H distribution' version.
-        completenessInt[nObs] = completeness[nObs].reduceMetric(completeness[nObs].metric.reduceFuncs['CumulativeH'])
-
-    Hmark = 22.0
-    for nObs in nObsList:
-        for c in [completeness[nObs], completenessInt[nObs]]:
-            summaryMetric = ValueAtHMetric(Hmark=Hmark)
-            c.setSummaryMetrics(summaryMetric)
-            c.computeSummaryStats()
-            label = "Completeness at H=%.1f: %.2f" %(Hmark, c.summaryValues['Value At H=%.1f' %Hmark])
-            c.setPlotDict({'label':label})
-            c.plot(plotFunc = moPlots.MetricVsH())
-            plt.axvline(Hmark, color='r', linestyle=':')
-            plt.axhline(c.summaryValues['Value At H=%.1f' %(Hmark)], color='r', linestyle='-')
-            plt.legend(loc=(0.1, 0.2))
-            plt.savefig(os.path.join(outDir, c.fileRoot + '_vsH' + '.png'), format='png')
+        discChances = discovery[nObs].childBundles['Discovery_N_Chances']
+        discChances.setSummaryMetrics([moMetrics.CompletenessMetric()])
+        discChances.computeSummaryStats()
+        completeness[nObs] = discChances.summaryValues['Completeness'][0]
+        hVals[nObs] = discChances.summaryValues['Completeness'][1]
+        Hidx = np.where(hVals[nObs] == Hmark)[0]
+        cval = completeness[nObs][Hidx]
+        plt.plot(hVals[nObs], completeness[nObs], label='%d per tracklet, %.2f @H=%.1f' %(nObs, cval, Hmark))
+    for nObs in ['4nights']:
+        discChances = discovery[nObs].childBundles['Discovery_N_Chances']
+        discChances.setSummaryMetrics([moMetrics.CompletenessMetric()])
+        discChances.computeSummaryStats()
+        completeness[nObs] = discChances.summaryValues['Completeness'][0]
+        hVals[nObs] = discChances.summaryValues['Completeness'][1]
+        Hidx = np.where(hVals[nObs] == Hmark)[0]
+        cval = completeness[nObs][Hidx]
+        plt.plot(hVals[nObs], completeness[nObs], label='%d pairs per track, %.2f @H=%.1f' %(nObs, cval, Hmark))
+    plt.axvline(Hmark, color='r', linestyle=':')
+    plt.legend(loc=(0.1, 0.2))
+    plt.savefig(os.path.join(outDir, 'completeness.png'), format='png')
 
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Python script to run MAF NEO completeness analysis')
-    parser.add_argument('dbFile', type=str, default=None,help="full file path to the opsim sqlite file")
-    parser.add_argument('--orbitfile', type=str, default='pha20141031.des', help="full file path to the orbit file")
+    parser.add_argument('--orbitFile', type=str, default='pha20141031.des', help="full file path to the orbit file")
     parser.add_argument("--outDir",type=str, default=None, help='Output directory for MAF outputs. Default opsimdb_orbitroot')
     parser.add_argument('--plotOnly', dest='plotOnly', action='store_true',
                         default=False, help="Reload the metric values from disk and re-plot them.")
+    parser.add_argument('dbFile', type=str, help="full file path to the opsim sqlite file")
     parser.set_defaults()
     args, extras = parser.parse_known_args()
 
-    orbitfile = args.orbitfile
+    orbitfile = args.orbitFile
     orbitroot = os.path.split(orbitfile)[-1].replace('.txt', '').replace('.des', '').replace('.dat', '')
     obsfile = orbitroot + '_allObs.txt'
     opsimdb = args.dbFile
